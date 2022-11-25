ebda6a03c143e0796a42f73972534ea02b7cc245
TheGalit <117729738+TheGalit@users.noreply.github.com>
2022-11-25 11:55:54 +0000

    Add files via upload

---
+++ learningAgent.py
@@ -0,0 +1,61 @@
+# Ed, Kat, Galit
+# ReinforceAlgorithm Class: Solver.
+
+import numpy as np #repeated
+import torch
+import torch.nn as nn
+from torch.distributions import Categorical
+import sys
+# printoptions: output limited to 2 digits after decimal point
+np.set_printoptions(precision=2, suppress=False)
+
+5
+class ReinforceAlgorithm():
+    """
+        Model Solver.
+    """
+    #def __init__(self, Model, neuralNet, numberIterations, numberEpisodes, discountFactor) -> None:
+    def __init__(self, Model, Qtable, numberEpisodes, discountFactor) -> None:
+        self.env = Model
+        self.env.adversaryReturns = np.zeros(numberEpisodes)
+        self.returns = np.zeros( numberEpisodes)
+        self.numberEpisodes = numberEpisodes
+        self.episodesMemory = list()
+        self.gamma = discountFactor
+        self.Qtable=Qtable
+        self.numStates=len(Qtable) #Qtable dimen - number of states x number of actions
+        self.numActions=len(Qtable[0])
+        #self.numberIterations = numberIterations
+        #self.neuralNetwork = neuralNet
+        #self.policy = None
+        #self.optim = None
+        #self.bestPolicy=None
+        #self.bestAverageRetu = 0
+
+        """
+        This function will eventually choose and adversary agent to play against
+        and according to that return an action played by that adversary. 
+        This could be chosen according to some existing equilibruim dist.
+        """
+    def chooseAdver(self, st):
+        return (400-st +self.env.costs[1])/2  #myopic
+
+    """
+    Now the Q-learning itself 
+    """
+    def  solver(self):
+        for episode in range(self.numberEpisodes):
+            #state, reward, done = self.env.reset()
+            state=np.random.randint(200-(self.numStates)/2,200+(self.numStates)/2+1)
+            monprice=(state + self.env.costs[0]) / 2
+            action=np.random.randint(monprice-self.numActions, monprice+1)
+            advAction=self.chooseAdver(state)
+
+            reward=(state-action)*(action-self.env.costs[0])
+            next_state=state+.5*(advAction-action)
+            opt_value_next=max(self.Qtable[next_state-200+self.numStates/2])
+           #CHECK INDICES!
+            #WRITE ALPHA FUNCTION
+            qvalue=self.Qtable[state,action](reward+self.gamma*opt_value_next)
+
+
