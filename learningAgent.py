01015e90f4d372687698282c11456cd0e5656085
TheGalit <117729738+TheGalit@users.noreply.github.com>
2022-11-26 11:08:28 +0000

    Add files via upload

--- learningAgent.py
+++ learningAgent.py
@@ -24,7 +24,11 @@ class ReinforceAlgorithm():
         self.gamma = discountFactor
         self.Qtable=Qtable
         self.numStates=len(Qtable) #Qtable dimen - number of states x number of actions
+        if self.numStates % 2 ==1 :
+            print('num of states should be even')
         self.numActions=len(Qtable[0])
+        if self.numActions % 2 ==1 :
+            print('num of actions should be even')
         #self.numberIterations = numberIterations
         #self.neuralNetwork = neuralNet
         #self.policy = None
@@ -40,22 +44,33 @@ class ReinforceAlgorithm():
     def chooseAdver(self, st):
         return (400-st +self.env.costs[1])/2  #myopic
 
+    def StateInd(self, state):                 # computes state index in Qtable
+        return state -(200-self.numStates/2)
+
+    def ActionInd(self, monprice, action):      # computes action index in Qtable
+        return action-(monprice-self.numActions+1)
+
+    def alpha_n(self, n):                       # defines the Qlearning rate
+        return 1/n
     """
     Now the Q-learning itself 
     """
     def  solver(self):
         for episode in range(self.numberEpisodes):
             #state, reward, done = self.env.reset()
-            state=np.random.randint(200-(self.numStates)/2,200+(self.numStates)/2+1)
+            state=np.random.randint(200-(self.numStates)/2,200+(self.numStates)/2)
             monprice=(state + self.env.costs[0]) / 2
-            action=np.random.randint(monprice-self.numActions, monprice+1)
+            action=np.random.randint(monprice-self.numActions+1, monprice+1)
             advAction=self.chooseAdver(state)
-
             reward=(state-action)*(action-self.env.costs[0])
             next_state=state+.5*(advAction-action)
-            opt_value_next=max(self.Qtable[next_state-200+self.numStates/2])
-           #CHECK INDICES!
-            #WRITE ALPHA FUNCTION
-            qvalue=self.Qtable[state,action](reward+self.gamma*opt_value_next)
+            opt_value_next=max(self.Qtable[self.StateInd(next_state)])
+
+            # updating the Qtable
+            qvalue=(1-self.alpha_n(episode))*\
+                   self.Qtable[self.StateInd(state),self.ActionInd(monprice, action)]\
+                   +self.alpha_n(episode)*\
+                   (reward+self.gamma*opt_value_next)
+            self.Qtable[self.StateInd(state),self.ActionInd(monprice, action)]=qvalue
 
 
