d4c6814ca726480d0bf1f376d0e9ecb4c09b4f83
Clem <clemvonstengel@gmail.com>
2022-12-19 17:01:08 +0000

    controller for optimising Imit

--- clem_experiments/controller.py
+++ clem_experiments/controller.py
@@ -1,50 +1,45 @@
 from duopoly_game import DuopolyGame
-from learning_guess import FiniteDifferenceGuess
-from hard_coded_agents import Myopic, Guess
+from agent import FiniteDifferenceAgent
+from hard_coded_agents import Myopic, Imit, Guess
 
 game = DuopolyGame(25)
 
-def train_low_cost(learning_agent, opponent, epsilon = 1e-7):
-    for _ in range(100):        
-        learning_agent.increment_learnable_parameter(-epsilon)
-        profit1, _ = game.run(learning_agent, opponent)
+def train_low_cost(learning_agent, opponent, epsilon = 1e-4):
+    for _ in range(100):
+        profit, _ = game.run(learning_agent, opponent)
+
+        for parameter in learning_agent.learnable_parameters:
+            learning_agent.increment_parameter(parameter, epsilon)
+            profit_delta, _ = game.run(learning_agent, opponent)
+            learning_agent.gradients[parameter] = (profit_delta - profit) / epsilon
+            learning_agent.increment_parameter(parameter, -epsilon)
         
-        learning_agent.increment_learnable_parameter(2*epsilon)
-        profit2, _ = game.run(learning_agent, opponent)
-        
-        learning_agent.increment_learnable_parameter(-epsilon)
         learning_agent.reset()
-        
-        learning_agent.gradient = (profit2 - profit1) / (2*epsilon)
         learning_agent.step()
 
-def train_high_cost(learning_agent, opponent, epsilon = 1e-7):
-    for _ in range(100):        
-        learning_agent.increment_learnable_parameter(-epsilon)
-        _, profit1 = game.run(opponent, learning_agent)
-        
-        learning_agent.increment_learnable_parameter(2*epsilon)
-        _, profit2 = game.run(opponent, learning_agent)
-        
-        learning_agent.increment_learnable_parameter(-epsilon)
+def train_high_cost(learning_agent, opponent, epsilon = 1e-5):
+    for _ in range(100):
+        _, profit = game.run(opponent, learning_agent)
+
+        for parameter in learning_agent.learnable_parameters:
+            learning_agent.increment_parameter(parameter, epsilon)
+            _, profit_delta, = game.run(opponent, learning_agent)
+            learning_agent.gradients[parameter] = (profit_delta - profit) / epsilon
+            learning_agent.increment_parameter(parameter, -epsilon)
+            #print(parameter, learning_agent.agent.parameters[parameter], profit, profit_delta, (profit_delta - profit) / epsilon)
         learning_agent.reset()
-        
-        learning_agent.gradient = (profit2 - profit1) / (2*epsilon)
         learning_agent.step()
 
-trainee = FiniteDifferenceGuess(Guess(125, 207, 61, 125), 0.1)
-
-nice_trainer = Myopic()
-mean_trainer = Guess(130, 193, 75, 130)
-
-trainee2 = FiniteDifferenceGuess(Guess(130, 193, 75, 130), 0.1)
-
-for _ in range(5):
-    print(game.run(trainee, trainee2))
-    train_low_cost(trainee, trainee2)
-    print(game.run(trainee, trainee2))
-    train_high_cost(trainee2, trainee)
+low_cost = FiniteDifferenceAgent(Imit({"start_price": 110}), ["start_price"], 0.1)
+high_cost = FiniteDifferenceAgent(Imit({"start_price": 130}), ["start_price"], 0.1)
 
 
-print(trainee.agent.start_price)
-print(trainee2.agent.start_price)
+print(game.run(low_cost, high_cost))
+train_low_cost(low_cost, high_cost)
+print(game.run(low_cost, high_cost))
+train_high_cost(high_cost, low_cost)
+print(game.run(low_cost, high_cost))
+
+
+print(low_cost.agent.parameters)
+print(high_cost.agent.parameters)
