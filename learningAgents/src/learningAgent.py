bdde09e85aa9bfb2195c1cc5037e2b52e03d1564
Edward Plumb <116684413+EdwardPlumb@users.noreply.github.com>
2022-12-22 15:48:38 +0000

    Updated Files

---
+++ learningAgents/src/learningAgent.py
@@ -0,0 +1,90 @@
+# Ed, Kat, Galit
+# ReinforceAlgorithm Class: Solver.
+
+import numpy as np #repeated
+#import torch
+#import torch.nn as nn
+#from torch.distributions import Categorical
+import sys
+# printoptions: output limited to 2 digits after decimal point
+np.set_printoptions(precision=2, suppress=False)
+
+5
+class ReinforceAlgorithm():
+    """
+        Model Solver.
+    """
+    #def __init__(self, Model, neuralNet, numberIterations, numberEpisodes, discountFactor) -> None:
+    def __init__(self, Model, Qtable, numberEpisodes, discountFactor) -> None:
+        self.env = Model
+        self.env.adversaryReturns = np.zeros(numberEpisodes)
+        self.returns = np.zeros( numberEpisodes)
+        self.numberEpisodes = numberEpisodes
+        self.episodesMemory = list()
+        self.gamma = discountFactor
+        self.Qtable=Qtable
+        self.numStates=len(Qtable) #Qtable dimen - number of states x number of actions
+        if self.numStates % 2 ==1 :
+            print('num of states should be even')
+        self.numActions=len(Qtable[0])
+        if self.numActions % 2 ==1 :
+            print('num of actions should be even')
+
+        self.lowestState = int(200-(self.numStates)/2)
+        self.highestState = int(200+(self.numStates)/2 - 1)
+
+        #self.numberIterations = numberIterations
+        #self.neuralNetwork = neuralNet
+        #self.policy = None
+        #self.optim = None
+        #self.bestPolicy=None
+        #self.bestAverageRetu = 0
+
+        """
+        This function will eventually choose and adversary agent to play against
+        and according to that return an action played by that adversary. 
+        This could be chosen according to some existing equilibruim dist.
+        """
+    def chooseAdver(self, st):
+        return int((400-st +self.env.costs[1])/2) + 1 #myopic
+
+    def StateInd(self, state):                 # computes state index in Qtable
+        return int(state -(200-self.numStates/2))
+        #return min(int(state -(200-self.numStates/2)), self.numStates - 1)
+
+    def ActionInd(self, monprice, action):      # computes action index in Qtable
+        return int(action-(monprice-self.numActions+1))
+
+    def alpha_n(self, n):                       # defines the Qlearning rate
+        return 500/(n+600)
+    """
+    Now the Q-learning itself 
+    """
+    def  solver(self):
+        for episode in range(self.numberEpisodes):
+
+            state = np.random.randint(self.lowestState, self.highestState + 1)
+
+            monprice = int((state + self.env.costs[0]) / 2) + 1
+            action = np.random.randint(monprice-self.numActions+1, monprice+1)
+            advAction = self.chooseAdver(state)
+            reward = (state-action)*(action-self.env.costs[0])
+
+            #print(state,action,monprice)
+
+            next_state = int(state+.5*(advAction-action))
+
+            #print(state, action, monprice, next_state)
+            #print(self.StateInd(state), self.ActionInd(monprice, action), monprice, self.StateInd(next_state))
+            #print(self.highestState)
+
+            opt_value_next = max(self.Qtable[self.StateInd(next_state)])
+
+            # updating the Qtable
+            qvalue = (1-self.alpha_n(episode)) * \
+                   self.Qtable[self.StateInd(state), self.ActionInd(monprice, action)]\
+                   + self.alpha_n(episode) * \
+                   ((1-self.gamma)*reward + self.gamma*opt_value_next)
+            self.Qtable[self.StateInd(state), self.ActionInd(monprice, action)]=qvalue
+
+
---
+++ learningAgents/src/main (2).py
@@ -0,0 +1,59 @@
+
+from learningAgent import ReinforceAlgorithm
+from environment import Model, AdversaryModes
+import numpy as np
+
+np.random.seed(10)
+agent_cost = 57
+adv_cost = 71
+
+game = Model(totalDemand = 400,
+               tupleCosts = (agent_cost, adv_cost),
+               totalStages = 25,
+               initState = [400/2,0], adversaryMode=AdversaryModes.myopic)
+
+num_Actions = 6
+num_States = abs(adv_cost - agent_cost) + 2 * num_Actions + 2
+gamma = 0.9
+print(num_States)
+
+Qtable = np.zeros((num_States, num_Actions))
+Qtable_error = np.zeros((num_States, num_Actions))
+
+algorithm = ReinforceAlgorithm(game, Qtable, numberEpisodes = 1000000, discountFactor = gamma)
+
+algorithm.solver()
+
+# printing Qtable for excel
+for s in range(num_States):
+    for a in range(num_Actions):
+        print("%.5f, " % Qtable[s,a], end="")
+    print("")
+
+# Checking the convergence of the Qtable
+
+for s in range(num_States):
+    for a in range(num_Actions):
+        lowestState = int(200-(num_States)/2)
+        highestState = int(200+(num_States)/2 - 1)
+        state = s + lowestState
+
+        monopoly_price = int((state + agent_cost)/2) + 1
+        action = a + monopoly_price - num_Actions + 1
+
+        reward = (state - action) * (action - agent_cost)
+        adv_action = int((400 -state + adv_cost)/2) + 1
+        next_state = int(state + (adv_action - action)/2)
+        #print(state,monopoly_price,action,reward,adv_action, next_state)
+
+        ns = next_state - lowestState
+        opt_value_next = max(Qtable[ns])
+        new_value = (1-gamma)*reward + gamma * opt_value_next
+        Qtable_error[s,a] = (new_value - Qtable[s,a])/new_value
+
+# Printing Qtable error
+for s in range(num_States):
+    for a in range(num_Actions):
+        #print(Qtable[s,a],",", end="")
+        print("%.5f, " % Qtable_error[s,a], end="")
+    print("")
