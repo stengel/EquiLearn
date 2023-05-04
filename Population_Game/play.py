1305e5a22ab42f02256adabfbb4628f43aa3f161
EdwardPlumb <116684413+EdwardPlumb@users.noreply.github.com>
2023-05-04 14:58:44 +0100

    New_Population_Game

--- Population_Game/play.py
+++ Population_Game/play.py
@@ -41,7 +41,31 @@ class Tournament():
             low_demand, high_demand, low_reward, high_reward, low_previous_action_index, high_previous_action_index = self.update_demands_rewards_prices(low_demand, high_demand, low_action, high_action, self.costs, number_actions)
             low_utility += (low_reward * delta)
             high_utility += (high_reward * delta)
+            delta *= self.discount_factor
         return [(low_utility / self.number_rounds), (high_utility / self.number_rounds)]
+    
+    def return_trajectory(self, low_cost_strategy, high_cost_strategy):
+        trajectory = list()
+        # delta = 1
+        # low_utility = 0
+        # high_utility = 0
+        low_demand, high_demand = self.initial_demands
+        low_previous_action_index = 0
+        high_previous_action_index = 0
+        low_number_actions = len(low_cost_strategy[int(low_demand), :, 0])
+        high_number_actions = len(high_cost_strategy[int(high_demand), :, 0])
+        number_actions = [low_number_actions, high_number_actions]
+        for round_ in range(self.number_rounds):
+            low_demand = int(low_demand)
+            high_demand = int(high_demand)
+            trajectory.append((low_demand, high_demand))
+            low_action = low_cost_strategy[low_demand][low_previous_action_index][round_]
+            high_action = high_cost_strategy[high_demand][high_previous_action_index][round_]
+            low_demand, high_demand, low_reward, high_reward, low_previous_action_index, high_previous_action_index = self.update_demands_rewards_prices(low_demand, high_demand, low_action, high_action, self.costs, number_actions)
+            # low_utility += (low_reward * delta)
+            # high_utility += (high_reward * delta)
+            # delta *= self.discount_factor
+        return trajectory
         
         
     def update_demands_rewards_prices(self, low_demand, high_demand, low_action, high_action, costs, number_actions):
